eb5232e6b945b7fb067d81e7b5083741d7ef3b4f
castilloxavie <xcastillovaron93@gmail.com>
2026-02-27 20:05:53 -0500

    ajustes cord

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 # Configuración CORS para permitir peticiones desde el frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://gestion-concesionario-inventario-c6zvccgr9.vercel.app"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://gestion-concesionario-inventario.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
